328fa9b359b6a5abd01e0217e20c8d24713f5abf
Timothy Perrett <458+timperrett@users.noreply.github.com>
2019-01-01 09:41:48 -0800

    needs central requirements plugin (#2)

---
+++ project/CentralRequirementsPlugin.scala
@@ -0,0 +1,60 @@
+//: ----------------------------------------------------------------------------
+//: Copyright (C) 2017 Verizon.  All Rights Reserved.
+//:
+//:   Licensed under the Apache License, Version 2.0 (the "License");
+//:   you may not use this file except in compliance with the License.
+//:   You may obtain a copy of the License at
+//:
+//:       http://www.apache.org/licenses/LICENSE-2.0
+//:
+//:   Unless required by applicable law or agreed to in writing, software
+//:   distributed under the License is distributed on an "AS IS" BASIS,
+//:   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//:   See the License for the specific language governing permissions and
+//:   limitations under the License.
+//:
+//: ----------------------------------------------------------------------------
+package verizon.build
+
+import sbt._, Keys._
+import xerial.sbt.Sonatype.autoImport.sonatypeProfileName
+
+object CentralRequirementsPlugin extends AutoPlugin {
+
+  override def trigger = allRequirements
+
+  override def requires = RigPlugin
+
+  override lazy val projectSettings = Seq(
+    publishTo := Some("releases"  at "https://oss.sonatype.org/service/local/staging/deploy/maven2"),
+    sonatypeProfileName := "io.getnelson",
+    pomExtra in Global := {
+      <developers>
+        <developer>
+          <id>timperrett</id>
+          <name>Timothy Perrett</name>
+          <url>http://github.com/timperrett</url>
+        </developer>
+        <developer>
+          <id>stew</id>
+          <name>Stew O'Connor</name>
+          <url>http://github.com/stew</url>
+        </developer>
+        <developer>
+          <id>rossabaker</id>
+          <name>Ross Baker</name>
+          <url>http://github.com/rossabaker</url>
+        </developer>
+        <developer>
+          <id>ceedubs</id>
+          <name>Cody Allen</name>
+          <url>http://github.com/ceedubs</url>
+        </developer>
+      </developers>
+    },
+    licenses := Seq("Apache-2.0" -> url("https://www.apache.org/licenses/LICENSE-2.0.html")),
+    homepage := Some(url("http://getnelson.io/")),
+    scmInfo := Some(ScmInfo(url("https://github.com/getnelson/platypus"),
+                                "git@github.com:getnelson/platypus.git"))
+  )
+}
